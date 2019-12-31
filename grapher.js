05017e3fa78bd2536cdd80aecfa574bc4c6d0b82
skribbliohints <skribbliohints@gmail.com>
2019-12-31 17:37:19 -0600

    Make time axis not start from 0

--- grapher.js
+++ grapher.js
@@ -1,4 +1,31 @@
-
+function getAxesConfigurationForChartJS(axis_key, axis_label, data) {
+  // axis_key: either 'x' or 'y'
+  // axis_label: the string to display for the axis
+  // data: array of objects of the form {x, y, label} where label is optional
+  let max = -Infinity;
+  let min = Infinity;
+  let sum = 0;
+  for(const element of data) {
+    const value = element[axis_key];
+    sum += value; // value better be a number
+    if(value < min) { min = value; }
+    if(value > max) { max = value; }
+  }
+  const start_from_zero = Math.abs((max - min) / (sum / data.length)) > 0.01;
+  const configuration_at_zero = {
+    scaleLabel: {
+    display: true,
+      labelString: axis_label
+    }
+  };
+  if(start_from_zero) {
+    configuration_at_zero.ticks = {
+      min: 0
+    };
+  }
+  return [ configuration_at_zero ];
+}
+  
 
 function getGraph(data, horizontal_axis_label, vertical_axis_label) {
   // data should be an array of arrays.  Each subarray should be [x,y,label].  label is optional.
@@ -6,12 +33,7 @@ function getGraph(data, horizontal_axis_label, vertical_axis_label) {
   const canvas = document.createElement('canvas');
   container.appendChild(canvas);
   const ctx = canvas.getContext('2d');
-  
-  Chart.scaleService.updateScaleDefaults('linear', {
-    ticks: {
-      min: 0
-    }
-  });
+
   Chart.defaults.global.legend.labels.usePointStyle = true;
   
   const formatted_data = [];
@@ -36,18 +58,8 @@ function getGraph(data, horizontal_axis_label, vertical_axis_label) {
     data: test_data,
     options: {
       scales: {
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: horizontal_axis_label
-          }
-        }],
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: vertical_axis_label
-          }
-        }]
+        xAxes: getAxesConfigurationForChartJS('x', horizontal_axis_label, formatted_data),
+        yAxes: getAxesConfigurationForChartJS('y', vertical_axis_label, formatted_data)
       },
       tooltips: {
         enabled: true,
